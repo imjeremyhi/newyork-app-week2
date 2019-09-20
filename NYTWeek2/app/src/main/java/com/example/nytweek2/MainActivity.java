dd82c3d5991f2fc28e24b469250f584b17e2247f
unknown <ari.mouzakis1@gmail.com>
2019-09-21 02:03:37 +1000

    minor changes made

--- NYTWeek2/app/src/main/java/com/example/nytweek2/MainActivity.java
+++ NYTWeek2/app/src/main/java/com/example/nytweek2/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.nytweek2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView bookmarkButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageView bookmarkButton = findViewById(R.id.newsSaveButton);
+        bookmarkButton1 = findViewById(R.id.newsSaveButton1);
+
+        //One way of implementing OnClick Listener
+        bookmarkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Click Registered", Toast.LENGTH_SHORT).show();
+                if(bookmarkButton.getDrawable().getConstantState() == getResources().getDrawable(R.drawable.ic_bookmark_border_black_24dp).getConstantState()) {
+                    bookmarkButton.setImageResource(R.drawable.ic_bookmark_black_24dp);
+                } else {
+                    bookmarkButton.setImageResource(R.drawable.ic_bookmark_border_black_24dp);
+                }
+            }
+        });
+
+    }
+
+    //Other way of implementing OnClick Listener
+    public void saveNewsArticle(View view) {
+        Toast.makeText(getApplicationContext(), "Click Registered", Toast.LENGTH_SHORT).show();
+        if(bookmarkButton1.getDrawable().getConstantState() == getResources().getDrawable(R.drawable.ic_bookmark_border_black_24dp).getConstantState()) {
+            bookmarkButton1.setImageResource(R.drawable.ic_bookmark_black_24dp);
+        } else {
+            bookmarkButton1.setImageResource(R.drawable.ic_bookmark_border_black_24dp);
+        }
     }
 }
